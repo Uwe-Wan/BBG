951a938853177b73c17b61f71def6a238ea427a6
Niklas Wolf <niwolfmailcenter@gmx.de>
2021-01-02 18:24:27 +0100

    add android app project

---
+++ AndroidApp/app/src/main/java/com/bbg/bbg_app/ui/login/LoginResult.kt
@@ -0,0 +1,9 @@
+package com.bbg.bbg_app.ui.login
+
+/**
+ * Authentication result : success (user details) or error message.
+ */
+data class LoginResult(
+        val success: LoggedInUserView? = null,
+        val error: Int? = null
+)
