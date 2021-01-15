adc7414b28058181252380377b3b5a94cc936542
Uwe <stochastikgott@web.de>
2021-01-15 22:49:57 +0100

    BBG-19: added a gradle project and a entities module

---
+++ UserManagementService/Entities/module-info.java
@@ -0,0 +1,3 @@
+module BBG.Entities {
+    exports BBG.Entities;
+}
---
+++ UserManagementService/Entities/src/User.java
@@ -0,0 +1,34 @@
+public class User{
+    private int id;
+    private String name;
+    private String eMail;
+    private int passwordHash;
+
+    public int getId(){
+        return id;
+    }
+
+    public String getName(){
+        return name;
+    }
+
+    public void setName(String newName){
+        name = newName;
+    }
+
+    public String getEMail(){
+        return eMail;
+    }
+
+    public void setEMail(String newEMail){
+        eMail = newEMail;
+    }
+
+    public int getPasswordHash(){
+        return passwordHash;
+    }
+
+    public void setPasswordHash(String newPasswordHash){
+        passwordHash = newPasswordHash;
+    }
+}
